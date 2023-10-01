10f4046489d1904b04c655788cf093e33e1171de
Gabriel Oliveira Quaresma <gabriel.oliveira.quaresma@gmail.com>
2023-10-01 20:11:14 -0300

    React + Tailwind Config

---
+++ studHelp/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}',],
+  theme: {
+    extend: {
+      colors: {
+        'page-blue' : '#7DC2D9'
+      }
+    },
+  },
+  plugins: [],
+}
+
