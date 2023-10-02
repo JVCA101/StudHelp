613517e8fab4c9960b89e79a6b9bcb595cc7a0e8
Gabriel Oliveira Quaresma <gabriel.oliveira.quaresma@gmail.com>
2023-10-02 20:01:32 -0300

    Extensions config

--- studHelp/src/SideBar.tsx
+++ studHelp/src/SideBar.tsx
@@ -1,24 +1,34 @@
 import { useState } from "react";
 import React from "react";
 
-
-function App() {
+function SideBar() {
   const [open, setOpen] = useState(true);
-  return (   
-   <div className='flex'> 
-      <div className={`${open ? 'w-1/5' : 'w-28'} duration-300 h-screen bg-gradient-to-b from-blue-50  via-mid-bar  opacity-40 to-blue-50 relative`}>  
-          <img 
-            src='./src/assets/right-arrow-angle 2.svg'
-            className={`absolute cursor-pointer -right-2 w-10 top-2 ${open && "rotate-180"}`}
-            onClick={()=>setOpen(!open)}
+  return (
+    <div className="flex">
+      <div
+        className={`${
+          open ? "w-1/5" : "w-28"
+        } duration-300 h-screen bg-gradient-to-b from-blue-50  via-mid-bar  opacity-40 to-blue-50 relative`}
+      >
+        <img
+          src="./src/assets/right-arrow-angle 2.svg"
+          className={`absolute cursor-pointer -right-2 w-10 top-2 ${
+            open && "rotate-180"
+          }`}
+          onClick={() => setOpen(!open)}
+        />
+        <div>
+          <img
+            src="./src/assets/logo.svg"
+            className={`cursor-pointer duration-500 w-20`}
           />
-         <div>
-          <img src='./src/assets/logo.svg' className={`cursor-pointer duration-500 w-20`} />
-          <h1 className={`text-black origin-left font-sans`}>StudHelp</h1>
-         </div>
-      </div> 
-   </div>
-  );  
+          <h1 className={`text-black origin-left font-sans left-3`}>
+            StudHelp
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App;
+export default SideBar;
