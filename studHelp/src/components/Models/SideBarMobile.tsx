40917c52f83238d31ebcfea1ffc913681c157485
quarerma <gabriel.oliveira.quaresma@gmail.com>
2023-11-24 20:10:23 -0300

    SideBar Mobile animação

--- studHelp/src/components/Models/SideBarMobile.tsx
+++ studHelp/src/components/Models/SideBarMobile.tsx
@@ -17,12 +17,20 @@ export const MobileSideBar = ({ Menus }: Props) => {
           onClick={() => setOpen(!open)}
         />
       </div>
-      <ul className="md:hidden absolute z-50 bottom-[12%] ml-4 left-0 p-3">
-        {open &&
-          Menus.map((menu, index) => (
-            <li key={index} className="">
-              <Link to={menu.href}>
-                <div className="flex gap-x-10">
+      <ul className="md:hidden absolute z-50 bottom-[12%] ml-5 left-0 p-3">
+        {Menus.map((menu, index) => (
+          <li key={index} className=" ">
+            <Link to={menu.href}>
+              <div
+                className={`flex gap-x-10 duration-500 transform ${
+                  open ? "" : " translate-y-96 opacity-0   pointer-events-none"
+                } `}
+              >
+                <div
+                  className={`flex gap-x-10  ${
+                    !open && " duration-1000 hidden"
+                  } `}
+                >
                   <img
                     className="w-16 scale-110 mb-8 border origin-center border-black shadow-lg border-opacity-50 bg-blue-400 rounded-full"
                     src={`./src/assets/${menu.src}.svg`}
@@ -31,9 +39,10 @@ export const MobileSideBar = ({ Menus }: Props) => {
                     <strong>{menu.title}</strong>
                   </span>
                 </div>
-              </Link>
-            </li>
-          ))}
+              </div>
+            </Link>
+          </li>
+        ))}
       </ul>
       <div
         className={`md:hidden fixed inset-0 z-40 backdrop-blur-lg transition-opacity  ${
